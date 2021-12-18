3184db8586552f56637bb3bf16ad0055d4f4ed53
vinothvkr <vinothvkr@hotmail.com>
2021-12-18 12:22:58 +0530

    add home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  data: {preload: true}
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
